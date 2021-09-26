dd0a10df5439014ca9d7c1dca111d05e4af1cd47
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-09-26 18:10:03 +0200

    Create AppointmentImpl.java

---
+++ assignment/implementation/src/main/java/appointmentplanner/AppointmentImpl.java
@@ -0,0 +1,53 @@
+package appointmentplanner;
+
+import appointmentplanner.api.Appointment;
+import appointmentplanner.api.AppointmentData;
+import appointmentplanner.api.AppointmentRequest;
+import appointmentplanner.api.Priority;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class AppointmentImpl implements Appointment {
+
+
+
+    public AppointmentImpl(){
+
+    }
+
+    @Override
+    public Duration getDuration() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public Priority getPriority() {
+        return null;
+    }
+
+    @Override
+    public AppointmentData getAppointmentData() {
+        return null;
+    }
+
+    @Override
+    public AppointmentRequest getRequest() {
+        return null;
+    }
+
+    @Override
+    public Instant getStart() {
+        return null;
+    }
+
+    @Override
+    public Instant getEnd() {
+        return null;
+    }
+}
