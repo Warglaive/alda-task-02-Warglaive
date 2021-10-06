68ff4f749e4214aea5231a99dcae86a2613dae7f
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-10-06 16:09:57 +0200

    DoublyLinkedListTest

---
+++ assignment/implementation/src/test/java/appointmentplanner/DoublyLinkedListTest.java
@@ -0,0 +1,13 @@
+package appointmentplanner;
+
+import org.junit.jupiter.api.Test;
+
+public class DoublyLinkedListTest {
+    @Test
+    void test(){
+        DoublyLinkedList<Integer> lst = new DoublyLinkedList<Integer>();
+        lst.insertAt(1, 3);
+        lst.insertAt(1, 4);
+        System.out.println("sdadasdas");
+    }
+}
