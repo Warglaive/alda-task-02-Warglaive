d8a911f0638849b20abe8f0a4530275a24e35be5
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-11-02 14:28:03 +0200

    Update TimelineImpl.java

--- assignment/implementation/src/main/java/appointmentplanner/TimelineImpl.java
+++ assignment/implementation/src/main/java/appointmentplanner/TimelineImpl.java
@@ -11,98 +11,43 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class TimelineImpl implements Timeline {
-    /**
-     * Factory to create Timeslot
-     */
-    private AbstractAPFactory factory;
-    /**
-     * maintains a 'record' of both the allocated time slots and the (remaining) free time slots.
-     */
-    private int allocatedTimeSlots;
-    private int remainingFreeSlots;
-    /**
-     * Start and End Instants of Timeline
-     */
     private Instant start;
     private Instant end;
-    /**
-     * Defaults
-     */
-    private Instant defaultStart = LocalDay.now().ofLocalTime(LocalTime.of(0, 0));
-    private Instant defaultEnd = LocalDay.now().ofLocalTime(LocalTime.of(23, 59));
-    /**
-     * Doubly Linked List
-     */
-
-    private DoublyLinkedList<TimeSlot> timeLineAllocations;
-
-
-    private int nrOfAppointments;
+    private DoublyLinkedList<TimeSlot> appointments;
 
     public TimelineImpl(Instant start, Instant end) {
-        this.factory = new APFactory();
-        this.timeLineAllocations = new DoublyLinkedList<>();
+        this(start, end, new DoublyLinkedList<TimeSlot>());
+    }
+
+    public TimelineImpl(Instant start, Instant end, DoublyLinkedList<TimeSlot> appointments) {
         this.start = start;
         this.end = end;
-    }
+        this.appointments = appointments;
 
-    /**
-     * If Start and End are not specified -> use defaults
-     */
-    public TimelineImpl() {
-        this.start = this.defaultStart;
-        this.end = this.defaultEnd;
+        if (this.appointments.getSize() == 0) {
+            var factory = new APFactory();
+            this.appointments.addFront(factory.between(start, end));
+        }
     }
-
 
     @Override
     public int getNrOfAppointments() {
-        //get number of appointments from the LinkedListImpl with help of stream and lambda
         return 0;
     }
 
     @Override
     public Instant start() {
-        return this.start;
+        return null;
     }
 
     @Override
     public Instant end() {
-        return this.end;
+        return null;
     }
-
-    /**
-     * fields for timeSlot to ease usage
-     */
-    private Instant timeslotStartTime;
-    private Instant timeslotEndTime;
 
     @Override
     public Optional<Appointment> addAppointment(LocalDay forDay, AppointmentData appointment, TimePreference timepreference) {
-        //TODO: Add AppointmentData Priority check
-        if (timepreference.equals(TimePreference.EARLIEST)) {
-            if (this.timeLineAllocations.getSize() <= 1) {
-                //TODO: probably buggy, test
-                this.timeslotStartTime = this.start;
-                this.timeslotEndTime = this.start.plusSeconds(appointment.getDuration().toSeconds());
-                //TODO: Is start properly made?
-                LocalTime timeslotStartLocalTime = LocalDay.now().timeOfInstant(this.timeslotStartTime);
-                LocalTime timeslotEndLocalTime = LocalDay.now().timeOfInstant(this.timeslotEndTime);
-                var timeslot = this.factory.between(forDay, timeslotStartLocalTime, timeslotEndLocalTime);
-                //TODO: WHere
-                //Add current timeslot to the Timeline
-/*
-                this.timeLineAllocations.addNode(timeslot);
-*/
-                //TODO: Add appointment and return...blbalbal
-/*
-                return Optional.of(timeslot);
-*/
-                return Optional.empty();
-            }
-        }
         return Optional.empty();
-
     }
 
     @Override
