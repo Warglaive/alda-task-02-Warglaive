84cb9034586dbba884f1ead0c3e6a6ee25b8c7c4
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-11-04 13:06:30 +0100

    Update TimelineTest.java

--- assignment/implementation/src/test/java/appointmentplanner/TimelineTest.java
+++ assignment/implementation/src/test/java/appointmentplanner/TimelineTest.java
@@ -1,15 +1,44 @@
 package appointmentplanner;
 
-import org.assertj.core.api.Assertions;
+import appointmentplanner.api.*;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TimelineTest {
 
-    @Test
+    private TimeSlot timeSlot;
+
+    private Stream illegalAppointmentStream;
+    private Stream appointmentStream;
+    private DoublyLinkedList<TimeSlot> appointments;
+    private Instant start, end;
+    private Timeline illegalInstantiatedTimeline;
+    private TimeLineImpl instantiatedTimeline;
+
+    private AppointmentData mockedAppointmentData;
+    private AppointmentRequest mockedAppointmentRequest;
+    private TimeSlot mockedTimeSlot;
+    private AppointmentImpl appointment;
+    private LocalDay localDay;
+
     @BeforeEach
-    void constructorTest() {
-       // TimelineImpl timeline = new TimelineImpl();
-       // Assertions.assertThat(timeline).isExactlyInstanceOf(TimelineImpl.class);
+    public void setUp() {
+        var factory = new APFactory();
+        this.localDay = LocalDay.now();
+
     }
 }
